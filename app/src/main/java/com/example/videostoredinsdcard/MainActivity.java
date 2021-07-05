54e2dc321df9b0663dc516e7f08a8b1cdab1f33f
piYush <piyushojha577@gmail.com>
2021-07-05 13:11:56 +0530

    First Commit

---
+++ app/src/main/java/com/example/videostoredinsdcard/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.videostoredinsdcard;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+@RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
+public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<VideoModel> videoArrayList;
+    RecyclerView recyclerView;
+    public static final int PERMISSION_READ = 0;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        if (checkPermission()) {
+            externalMemoryAvailable(this);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public boolean externalMemoryAvailable(Activity context) {
+        File[] storages = ContextCompat.getExternalFilesDirs(context, null);
+        if (storages.length > 1 && storages[0] != null && storages[1] != null) {
+
+            videoArrayList = new ArrayList<>();
+            getVideos();
+
+            Toast.makeText(context, "SD Card Present", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            Toast.makeText(context, "No SD Card", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    //get video files from storage
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void getVideos() {
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+
+        Cursor cursor = contentResolver.query(uri, null, MediaStore.Video.Media.DATA + " like ? ",  new String[]{"%/" + "Video" + "/%"}, null);
+
+        //looping through all rows and adding to list
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE));
+                String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                VideoModel videoModel = new VideoModel();
+                videoModel.setVideoTitle(title);
+                videoModel.setVideoUri(Uri.parse(data));
+                videoModel.setVideoDuration(timeConversion(Long.parseLong(duration)));
+                videoArrayList.add(videoModel);
+
+            } while (cursor.moveToNext());
+        }
+
+      /*  for (int i = 0; videoArrayList !=null && i < videoArrayList.size(); i++) {
+            if (videoArrayList.get(i).videoUri.equals("/storage/23DA-19EB/Video/Aaaa.mp4"));
+            ///storage/23DA-19EB/Video/Aaaa.mp4
+
+            //if (videoArrayList.get(i).)
+        }*/
+
+        VideoAdapter adapter = new VideoAdapter(this, videoArrayList);
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new VideoAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int pos, View v) {
+                Intent intent = new Intent(getApplicationContext(), VideoPlayActivity.class);
+                intent.putExtra("pos", pos);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    //time conversion
+    public String timeConversion(long value) {
+        String videoTime;
+        int dur = (int) value;
+        int hrs = (dur / 3600000);
+        int mns = (dur / 60000) % 60000;
+        int scs = dur % 60000 / 1000;
+
+        if (hrs > 0) {
+            videoTime = String.format("%02d:%02d:%02d", hrs, mns, scs);
+        } else {
+            videoTime = String.format("%02d:%02d", mns, scs);
+        }
+        return videoTime;
+    }
+
+    //runtime storage permission
+    public boolean checkPermission() {
+        int READ_EXTERNAL_PERMISSION = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if ((READ_EXTERNAL_PERMISSION != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_READ);
+            return false;
+        }
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_READ: {
+                if (grantResults.length > 0 && permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(getApplicationContext(), "Please allow storage permission", Toast.LENGTH_LONG).show();
+                    } else {
+                        externalMemoryAvailable(this);
+                    }
+                }
+            }
+        }
+    }
+}
